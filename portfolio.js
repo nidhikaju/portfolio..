0d326f9eeb8571b956d78a9df3705f2d2cef52fd
nidhikaju <161727726+nidhikaju@users.noreply.github.com>
2024-09-28 00:00:19 +0530

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,35 @@
+const contactForm = document.getElementById('contactForm');
+document.getElementById("resumeButton").addEventListener("click", function() {
+    //window.open("C:/Resume.pdf", "_blank");
+    const link = document.createElement('a');
+    link.href = 'Resume.pdf';
+    link.download = 'Resume.pdf';
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+});
+
+contactForm.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const name = document.getElementById('name').value.trim();
+    const email = document.getElementById('email').value.trim();
+    const message = document.getElementById('message').value.trim();
+
+    if (!name || !email || !message) {
+        alert('Please fill out all fields.');
+        return;
+    }
+
+    if (!validateEmail(email)) {
+        alert('Please enter a valid email.');
+        return;
+    }
+    alert('Message sent successfully!');
+    contactForm.reset();
+});
+
+function validateEmail(email) {
+    const re = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    return re.test(String(email).toLowerCase());
+}
